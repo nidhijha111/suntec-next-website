c110898f5b4ff07f2d64708b795cb9204ab9c2ec
nidhi <nidhi.kumari@sapidblue.com>
2025-06-17 18:23:18 +0530

    updated

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,11 +1,11 @@
-"use client"; 
+"use client";
 
 import React from "react";
-import { useRouter } from "next/navigation"; 
-import Image from "next/image"; 
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Footer() {
-  const router = useRouter(); 
+  const router = useRouter();
 
   return (
     <footer className="footer">
@@ -92,26 +92,26 @@ export default function Footer() {
             <Image
               src="/assets/images/instagram_icon.png"
               alt="instagram icon"
-              width={24} 
+              width={24}
               height={24}
             />
             <Image
               src="/assets/images/facebook_icon.png"
               alt="facebook icon"
-              width={24} 
-              height={24} 
+              width={24}
+              height={24}
             />
             <Image
               src="/assets/images/linkdin_icon.png"
               alt="linkedin icon"
-              width={24} 
-              height={24} 
+              width={24}
+              height={24}
             />
             <Image
               src="/assets/images/youtube_icon.png"
               alt="youtube icon"
-              width={24} 
-              height={24} 
+              width={24}
+              height={24}
             />
           </div>
         </div>
@@ -121,82 +121,92 @@ export default function Footer() {
           <Image
             src="/assets/images/dot_icon.png"
             alt="dot icon"
-            width={5} 
-            height={5} 
-          />{" "}
-          Suntec Insurance Broking Private Limited{" "}
-          <span className="red">CIN : U74999HR2014PTC053454</span>, Registered
-          Office : B 206, 2nd Floor, Eastern Business District, Lal Bahadur
-          Shastri Marg, Bhandup West, Mumbai, Maharashtra 400078, Tel No. : 022
-          45197023 I Email ID : care@suntecinsurance.com
+            width={4}
+            height={4}
+          />
+          <div>
+            Suntec Insurance Broking Private Limited{" "}
+            <span className="red">CIN : U74999HR2014PTC053454</span>, Registered
+            Office : B 206, 2nd Floor, Eastern Business District, Lal Bahadur
+            Shastri Marg, Bhandup West, Mumbai, Maharashtra 400078, Tel No. :
+            022 45197023 I Email ID : care@suntecinsurance.com
+          </div>
         </div>
         <div className="spacing-top"></div>
         <div className="footer_info_text_wrapper">
           <Image
             src="/assets/images/dot_icon.png"
             alt="dot icon"
-            width={5} 
-            height={5} 
-          />{" "}
-          Suntec Insurance is registered as a Direct Insurance Broker (Life and
-          General) I Registration No.1044, Valid till 03/03/2028, Registration
-          Code IRDAI/DB1159/2024, License category : Direct Insurance Broker
-          (Life and General)
+            width={4}
+            height={4}
+          />
+          <div>
+            Suntec Insurance is registered as a Direct Insurance Broker (Life
+            and General) I Registration No.1044, Valid till 03/03/2028,
+            Registration Code IRDAI/DB1159/2024, License category : Direct
+            Insurance Broker (Life and General)
+          </div>
         </div>
         <div className="spacing-top"></div>
         <div className="footer_info_text_wrapper">
           <Image
             src="/assets/images/dot_icon.png"
             alt="dot icon"
-            width={5} 
-            height={5} 
+            width={4}
+            height={4}
           />{" "}
-          Disclaimer : Suntec Insurance Broking Private Limited acts solely as a
-          facilitator. The final decision on claim settlement rests entirely
-          with the respective insurer. For complete details on risk factors,
-          terms and conditions, please refer to the insurer’s sales brochure
-          before making a purchase. Standard terms and conditions apply.
+          <div>
+            Disclaimer : Suntec Insurance Broking Private Limited acts solely as
+            a facilitator. The final decision on claim settlement rests entirely
+            with the respective insurer. For complete details on risk factors,
+            terms and conditions, please refer to the insurer’s sales brochure
+            before making a purchase. Standard terms and conditions apply.
+          </div>
         </div>
         <div className="footer_info_text_wrapper">
           <Image
             src="/assets/images/dot_icon.png"
             alt="dot icon"
-            width={5} 
-            height={5} 
+            width={4}
+            height={4}
           />{" "}
-          Disclaimer : Suntec Insurance Broking Private Limited does not
-          endorse, rate or recommend any particular insurer or any insurance
-          product offered by any insurer.
+          <div>
+            Disclaimer : Suntec Insurance Broking Private Limited does not
+            endorse, rate or recommend any particular insurer or any insurance
+            product offered by any insurer.
+          </div>
         </div>
         <div className="footer_info_text_wrapper">
           <Image
             src="/assets/images/dot_icon.png"
             alt="dot icon"
-            width={5} 
-            height={5}
+            width={4}
+            height={4}
           />{" "}
-          Disclaimer : The information provided on our website is intended to
-          offer an overview of Insurance products and services. For complete and
-          detailed information, please refer to the respective insurer’s
-          official website and product documents.
+          <div>
+            Disclaimer : The information provided on our website is intended to
+            offer an overview of Insurance products and services. For complete
+            and detailed information, please refer to the respective insurer’s
+            official website and product documents.
+          </div>
         </div>
         <div className="spacing-top"></div>
         <div className="footer_info_text_wrapper">
           <Image
             src="/assets/images/dot_icon.png"
             alt="dot icon"
-            width={5} 
-            height={5} 
+            width={4}
+            height={4}
           />{" "}
-          Visitors are hereby informed that their information submitted on the
-          website may be shared with insurers.
+          <div>Visitors are hereby informed that their information submitted on the
+          website may be shared with insurers.</div>
         </div>
         <div className="footer_info_text_wrapper">
           BEWARE OF SPURIOUS PHONE CALLS AND FICTITIOUS/FRAUDULENT OFFERS
         </div>
       </div>
       <p className="copyright footer_info_text_wrapper">
-        © Copyright 2026 - 2025 suntecinsurance.com. All Rights Reserved I
+        © Copyright 2025 - 2026 suntecinsurance.com. All Rights Reserved I
         Sitemap I FAQ’S
       </p>
     </footer>
