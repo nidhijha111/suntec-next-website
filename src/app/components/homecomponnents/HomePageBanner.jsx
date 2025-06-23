7b39e2a07a6af94afe7b8b03d650ef89687653d3
nidhi <nidhi.kumari@sapidblue.com>
2025-06-23 17:47:05 +0530

    updated

--- src/app/components/homecomponnents/HomePageBanner.jsx
+++ src/app/components/homecomponnents/HomePageBanner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HomePageBanner() {
   return (
     <section className="banner_image_section">
-      <img src="./assets/images/home_page_banner_img.png" alt="banner image" />
+      <img src="./assets/images/home_page_banner_img.jpg" alt="banner image" />
     </section>
   );
 }
