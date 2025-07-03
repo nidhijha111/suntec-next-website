1916559218bba1d426e57b2de4fc64f608bcb352
nidhi <nidhi.kumari@sapidblue.com>
2025-07-03 18:11:17 +0530

    updated

--- src/app/components/termInsuranceComponents/whenShouldBuyInsurence.jsx
+++ src/app/components/termInsuranceComponents/whenShouldBuyInsurence.jsx
@@ -4,28 +4,28 @@ export default function WhenShouldBuyInsurence() {
   const benefitsData = [
     {
       number: "1",
-      title: "In your 20s (Age 20–29)",
+      title: <>In your 20s <br/> (Age 20–29)</>,
       description:
         "Starting early means locking in low premiums and long-term coverage while you're young and healthy. It’s a smart way to protect against debts and future financial risks.",
       variant: "yellow",
     },
     {
       number: "2",
-      title: "In your 30s  (Age 30–39)",
+      title: <>In your 30s  <br/>(Age 30–39)</>,
       description:
         "With growing responsibilities like family and home loans, term Insurance ensures financial security for your loved ones - at still affordable premiums.",
       variant: "blue",
     },
     {
       number: "3",
-      title: "In your 40s  (Age 40–49)",
+      title: <>In your 40s  <br/>(Age 40–49)</>,
       description:
         "With major expenses like education, healthcare, and loans, term Insurance ensures your family stays protected—even if premiums are higher.",
       variant: "yellow",
     },
     {
       number: "4",
-      title: <>In your 50s (Age 50–59)</>,
+      title: <>In your 50s <br/>(Age 50–59)</>,
       description:
         "With limited options and higher premiums, it’s wise to opt for short-term or whole life plans that align with your goals—especially while you're still in good health.",
       variant: "blue",
